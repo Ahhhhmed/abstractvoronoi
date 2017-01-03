63f75accf79fe6439fd40bdc6c7f218734310ede
Nenad <ahhhhmed@gmail.com>
2017-01-04 00:26:47 +0100

    Created Descriptor class.

---
+++ lib/descriptor.cpp
@@ -0,0 +1,6 @@
+#include "descriptor.h"
+
+Descriptor::Descriptor()
+{
+
+}
---
+++ lib/descriptor.h
@@ -0,0 +1,11 @@
+#ifndef DESCRIPTOR_H
+#define DESCRIPTOR_H
+
+
+class Descriptor
+{
+public:
+    Descriptor();
+};
+
+#endif // DESCRIPTOR_H
