e2777e5930ec46456fad40019657cb99787bf1ad
Nenad <ahhhhmed@gmail.com>
2017-01-04 19:08:39 +0100

    Added source to HistoryGraph.

--- lib/historygraph.h
+++ lib/historygraph.h
@@ -1,11 +1,15 @@
 #ifndef HISTORYGRAPH_H
 #define HISTORYGRAPH_H
 
+#include <vector>
+#include "historygraphnode.h"
 
 class HistoryGraph
 {
 public:
     HistoryGraph();
+private:
+    std::vector<HistoryGraphNode*> source;
 };
 
 #endif // HISTORYGRAPH_H
