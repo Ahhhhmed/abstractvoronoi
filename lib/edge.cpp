90028f945905fed86ac00004610ff3bc7c6a8ff7
Nenad <ahhhhmed@gmail.com>
2017-01-05 00:28:19 +0100

    Added fealds to Edge class.

--- lib/edge.cpp
+++ lib/edge.cpp
@@ -9,12 +9,67 @@ Edge::Edge(int p, int q, int r, int t)
 
 }
 
-Edge::Edge(int p, int q, int r, int t, Descriptor* d)
+Edge::Edge(int p, int q, int r, int t, Descriptor* d, Edge *twin, Edge *next, Edge *prev, Vertex *origin, int face)
     :p(p)
     ,q(q)
     ,r(r)
     ,t(t)
     ,d(d)
+    ,twin(twin)
+    ,next(next)
+    ,prev(prev)
+    ,origin(origin)
+    ,face(face)
 {
 
+}
+
+Vertex *Edge::getOrigin() const
+{
+    return origin;
+}
+
+void Edge::setOrigin(Vertex *value)
+{
+    origin = value;
+}
+
+Edge *Edge::getTwin() const
+{
+    return twin;
+}
+
+void Edge::setTwin(Edge *value)
+{
+    twin = value;
+}
+
+Edge *Edge::getNext() const
+{
+    return next;
+}
+
+void Edge::setNext(Edge *value)
+{
+    next = value;
+}
+
+Edge *Edge::getPrev() const
+{
+    return prev;
+}
+
+void Edge::setPrev(Edge *value)
+{
+    prev = value;
+}
+
+int Edge::getFace() const
+{
+    return face;
+}
+
+void Edge::setFace(int value)
+{
+    face = value;
 }
--- lib/edge.h
+++ lib/edge.h
@@ -3,14 +3,44 @@
 
 #include "descriptor.h"
 
+class Vertex;
+
 class Edge
 {
 public:
     Edge(int p, int q, int r, int t);
-    Edge(int p, int q, int r, int t, Descriptor* d);
+    Edge(int p,
+         int q,
+         int r,
+         int t,
+         Descriptor* d,
+         Edge* twin,
+         Edge* next,
+         Edge* prev,
+         Vertex* origin,
+         int face);
+
+    Vertex *getOrigin() const;
+    void setOrigin(Vertex *value);
+
+    Edge *getTwin() const;
+    void setTwin(Edge *value);
+
+    Edge *getNext() const;
+    void setNext(Edge *value);
+
+    Edge *getPrev() const;
+    void setPrev(Edge *value);
+
+    int getFace() const;
+    void setFace(int value);
+
 private:
     int p,q,r,t; // sites
     Descriptor* d;
+    Edge *twin, *next, *prev;
+    Vertex *origin;
+    int face;
 };
 
 #endif // EDGE_H
