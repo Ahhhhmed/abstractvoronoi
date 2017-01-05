27b7554f0a463dfb3d651e16b7ada4473363d550
Nenad <ahhhhmed@gmail.com>
2017-01-05 01:55:24 +0100

    Added fealds and partaly implemented initialize method in AbstractVoronoiDiagram class.

--- lib/abstractvoronoidiagram.cpp
+++ lib/abstractvoronoidiagram.cpp
@@ -1,6 +1,49 @@
 #include "abstractvoronoidiagram.h"
 
+#include <algorithm>
+
+AbstractVoronoiDiagram &AbstractVoronoiDiagram::instance()
+{
+    static AbstractVoronoiDiagram inst;
+
+    return inst;
+}
 
 AbstractVoronoiDiagram::AbstractVoronoiDiagram()
 {
+
+}
+
+BasicOperationProvider* AbstractVoronoiDiagram::getProvider() const
+{
+    return provider;
+}
+
+void AbstractVoronoiDiagram::initialize(BasicOperationProvider *provider)
+{
+    this->provider = provider;
+
+    size = provider->number_of_sites();
+
+    current_step = 3;
+
+    sites.resize(size);
+    std::iota(sites.begin(), sites.end(), 0);
+    std::random_shuffle(sites.begin(), sites.end());
+
+    history.source.clear();
+    diagram.edges.clear();
+    diagram.vertices.clear();
+
+    //intitialize starting structures...
+}
+
+PlanarGraph AbstractVoronoiDiagram::getDiagram() const
+{
+    return diagram;
+}
+
+HistoryGraph AbstractVoronoiDiagram::getHistory() const
+{
+    return history;
 }
--- lib/abstractvoronoidiagram.h
+++ lib/abstractvoronoidiagram.h
@@ -1,12 +1,37 @@
 #ifndef ABSTRACTVORONOIDIAGRAM_H
 #define ABSTRACTVORONOIDIAGRAM_H
 
+#include "historygraph.h"
+#include "planargraph.h"
+#include "basicoperationprovider.h"
+
+#include <vector>
 
 class AbstractVoronoiDiagram
 {
-
 public:
+    AbstractVoronoiDiagram(AbstractVoronoiDiagram const&) = delete;
+    void operator=(AbstractVoronoiDiagram const&) = delete;
+
+    static AbstractVoronoiDiagram &instance();
+
+    HistoryGraph getHistory() const;
+
+    PlanarGraph getDiagram() const;
+
+    BasicOperationProvider *getProvider() const;
+
+    void initialize(BasicOperationProvider* provider);
+
+private:
     AbstractVoronoiDiagram();
+
+    HistoryGraph history;
+    PlanarGraph diagram;
+    BasicOperationProvider* provider;
+
+    int current_step, size;
+    std::vector<int> sites;
 };
 
 #endif // ABSTRACTVORONOIDIAGRAM_H
