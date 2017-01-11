1c8eb1e05720895ef2b0bcc10abaea9ea6a75803
Nenad <ahhhhmed@gmail.com>
2017-01-11 19:33:30 +0100

    DrawEdge implemented.

--- app/voronoiprovider.cpp
+++ app/voronoiprovider.cpp
@@ -1,4 +1,5 @@
 #include "voronoiprovider.h"
+#include <tuple>
 
 VoronoiProvider::VoronoiProvider()
 {
@@ -17,12 +18,28 @@ int VoronoiProvider::number_of_sites()
 
 void VoronoiProvider::DrawEdge(Edge *edge, QPainter &painter)
 {
+    std::tuple<int,int,int,int> e = std::make_tuple(edge->p,edge->r,edge->q,edge->t);
+    QPointF center(160,160);
+    int radius(150);
+    QPointF top(160,10);
+    QPointF bot = QTransform().translate(center.x(),center.y()).rotate(180).translate(-center.x(),-center.y()).map(top);
+    QPointF botRight = QTransform().translate(center.x(),center.y()).rotate(120).translate(-center.x(),-center.y()).map(top);
+    QPointF botLeft = QTransform().translate(center.x(),center.y()).rotate(240).translate(-center.x(),-center.y()).map(top);
 
-    QPoint p(10, 10);
-    QPoint q(30, 30);
     if (edge->p == 0 || edge->q == 0){
-        painter.drawRect(QRectF(p,q));
+        painter.drawEllipse(center, radius, radius);
     } else {
-        painter.drawLine(p, q);
+        if(e == std::make_tuple(1,0,2,0) || e == std::make_tuple(2,0,1,0)){
+            painter.drawLine(top, bot);
+        }
+        if(e == std::make_tuple(1,0,2,3) || e == std::make_tuple(2,3,1,0) || e == std::make_tuple(2,0,1,3) || e == std::make_tuple(1,3,2,0)){
+            painter.drawLine(top, center);
+        }
+        if(e == std::make_tuple(1,0,3,2) || e == std::make_tuple(3,0,1,2) || e == std::make_tuple(1,2,3,0) || e == std::make_tuple(3,2,1,0)){
+            painter.drawLine(center, botLeft);
+        }
+        if(e == std::make_tuple(2,0,3,1) || e == std::make_tuple(2,1,3,0) || e == std::make_tuple(3,0,2,1) || e == std::make_tuple(3,1,2,0)){
+            painter.drawLine(center, botRight);
+        }
     }
 }
